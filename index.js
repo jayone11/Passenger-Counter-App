59fa33f630ae87e6f3cc6deaf856d79211fd8ec4
JB1 <28571886+jayone11@users.noreply.github.com>
2022-10-12 19:47:35 -0700

    created increment function

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-document.getElementById("counter").innerText = 7
+// initialize the count as 0
+// listen for clicks on the incr button
+// incr the count variable when btn is clicked
+// chg the counter in html to reflect the new count
+// create a func save() ro log the count when called
+
+let count = 0;
+let counter = document.getElementById("counter");
+
+function increment() {
+    count = count + 1;
+    counter.innerText = count;
+    console.log("Count = ", count);
+}
